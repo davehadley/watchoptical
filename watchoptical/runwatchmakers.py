0b5234d38b4dfeedd316ec40028817f1cf8db334
David Hadley <d.r.hadley@warwick.ac.uk>
2020-06-01 18:38:51 +0100

    make tests run.

--- watchoptical/runwatchmakers.py
+++ watchoptical/runwatchmakers.py
@@ -12,7 +12,7 @@ class WatchMakersScripts:
 
 
 def path_to_watchmakers_script() -> str:
-    return os.sep.join((os.env['WATCHENV'],
+    return os.sep.join((os.environ['WATCHENV'],
                         "watchmakers.py"
                         ))
 
@@ -22,7 +22,7 @@ def generatejobscripts(directory: str = None) -> WatchMakersScripts:
         directory = tempfile.mkdtemp(prefix="watchoptical_runwatchmakers")
     _run_watchmakers_script(directory=directory)
     scripts = tuple(glob.glob(os.path.sep.join((directory,
-                                                os.path.sep.join(["lassen", "job", "script_*.sh"])
+                                                os.path.sep.join(["job", "script_*.sh"])
                                                 ))))
     return WatchMakersScripts(scripts=scripts)
 
@@ -30,10 +30,10 @@ def generatejobscripts(directory: str = None) -> WatchMakersScripts:
 def _run_watchmakers_script(directory: str = None):
     if not os.path.exists(directory):
         os.makedirs(directory, exist_ok=True)
-    subprocess.check_call(["python",
+    subprocess.check_call(["python3",
                            path_to_watchmakers_script(),
                            "-m",
-                           "-j"
+                           "-j",
                            "--lassen"
                            ],
                           cwd=directory)
