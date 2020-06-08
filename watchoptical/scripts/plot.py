5fd3a75c8684f936025cd57070042725b1a6593d
David Hadley <d.r.hadley@warwick.ac.uk>
2020-06-08 15:42:35 +0100

    WMDataset tests pass.

---
+++ watchoptical/scripts/plot.py
@@ -0,0 +1,25 @@
+from argparse import ArgumentParser, Namespace
+
+from watchoptical.internal.utils import findfiles
+from watchoptical.internal.wmdataset import WatchmanDataset
+
+
+def parsecml() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument("inputfiles", nargs="+", type=str, default=["~/work/wm/data/testwatchoptical/attempt01/*_files_default/*/*.root"])
+    return parser.parse_args()
+
+
+def plot():
+    return
+
+
+def main():
+    args = parsecml()
+    dataset = WatchmanDataset(args.inputfiles)
+    plot()
+    return
+
+
+if __name__ == '__main__':
+    main()
