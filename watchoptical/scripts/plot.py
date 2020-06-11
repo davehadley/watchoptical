067ea6b2fd6768e6be2d9be882e70ba55376dda6
David Hadley <d.r.hadley@warwick.ac.uk>
2020-06-11 11:30:38 +0100

    A failed attempt to read WATCHMAN files with uproot. See https://github.com/scikit-hep/uproot/issues/495

--- watchoptical/scripts/plot.py
+++ watchoptical/scripts/plot.py
@@ -1,13 +1,38 @@
 from argparse import ArgumentParser, Namespace
 
+import uproot as uproot
+
 from watchoptical.internal.utils import findfiles
 from watchoptical.internal.wmdataset import WatchmanDataset
 
 
 def parsecml() -> Namespace:
     parser = ArgumentParser()
-    parser.add_argument("inputfiles", nargs="+", type=str, default=["~/work/wm/data/testwatchoptical/attempt01/*_files_default/*/*.root"])
+    parser.add_argument("--inputfiles", nargs="+", type=str, default=["~/work/wm/data/testwatchoptical/attempt01/*_files_default/*IBD_LIQUID_pn*/*.root"])
     return parser.parse_args()
+
+def load(dataset):
+    for f in dataset:
+        print(f)
+        g4file = uproot.open(f.g4file)
+        print(g4file)
+        #interpretation = uproot.asjagged(uproot.astable(uproot.asdtype([(' fBits', '>u8'), (' fUniqueID', '>u8'), ('id', '>i4'), ('charge', '>f4'), ('time', '>f8')], [('id', '<i4'), ('charge', '<f4'), ('time', '<f8')])), 10)
+        g4file.showstreamers()
+        g4file["T"].show()
+        #arr = g4file["T"]["ev"].array()
+        for offset in range(0, 1000):
+            try:
+                interpretation = uproot.asjagged(uproot.asdtype(
+                [(' fBits', '>u8'), (' fUniqueID', '>u8'), ('id', '>i4'), ('charge', '>f4'), ('time', '>f8')],
+                [('id', '<i4'), ('charge', '<f4'), ('time', '<f8')]), offset)
+                arr = g4file["T"]["ev.pmt"].array(interpretation)
+                print(arr)
+            except AssertionError:
+                pass
+        #df = uproot.daskframe(f.g4file, "T")
+        #print(df)
+        print(arr)
+        print(arr)
 
 
 def plot():
@@ -17,6 +42,7 @@ def plot():
 def main():
     args = parsecml()
     dataset = WatchmanDataset(args.inputfiles)
+    load(dataset)
     plot()
     return
 
