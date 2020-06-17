47fabcb31a6d427ebee496c6e97594a78bb19a62
David Hadley <d.r.hadley@warwick.ac.uk>
2020-06-17 17:40:08 +0100

    Add some info to the output file.

--- watchoptical/internal/watchopticalcpp/_watchopticalcpp.cxx
+++ watchoptical/internal/watchopticalcpp/_watchopticalcpp.cxx
@@ -18,19 +18,71 @@ void open(std::string filename) {
         [](std::vector<RAT::DS::EV>& events){ std::cout << "EventID:" << events.at(0).GetID() << ", totalQ:" << events.at(0).GetTotalCharge() << std::endl; });
 }
 
+std::vector<double> gettotalcharge(const std::vector<RAT::DS::EV>& ev) {
+    std::vector<double> result;
+    std::transform(ev.begin(), ev.end(), std::back_inserter(result),
+                   [](const RAT::DS::EV& e) {
+                       return e.GetTotalCharge();
+                   });
+    return result;
+}
+
+std::vector<double> getpmt_time(std::vector<RAT::DS::EV>& ev) {
+    std::vector<double> result;
+    for(auto& e : ev) {
+        for(int i = 0; i < e.GetPMTCount(); ++i) {
+            auto pmt = e.GetPMT(i);
+            result.push_back(pmt->GetTime());
+        }
+    }
+    return result;
+}
+
+std::vector<double> getpmt_charge(std::vector<RAT::DS::EV>& ev) {
+    std::vector<double> result;
+    for(auto& e : ev) {
+        for(int i = 0; i < e.GetPMTCount(); ++i) {
+            auto pmt = e.GetPMT(i);
+            result.push_back(pmt->GetCharge());
+        }
+    }
+    return result;
+}
+
+std::vector<double> getpmt_id(std::vector<RAT::DS::EV>& ev) {
+    std::vector<double> result;
+    for(auto& e : ev) {
+        for(int i = 0; i < e.GetPMTCount(); ++i) {
+            auto pmt = e.GetPMT(i);
+            result.push_back(pmt->GetID());
+        }
+    }
+    return result;
+}
+
+std::vector<double> getpmt_eventid(std::vector<RAT::DS::EV>& ev) {
+    std::vector<double> result;
+    for(auto& e : ev) {
+        for(int i = 0; i < e.GetPMTCount(); ++i) {
+            result.push_back(e.GetID());
+        }
+    }
+    return result;
+}
+
 void convert_ratpacbonsai_to_analysis(std::string ratpac, std::string bonsai, std::string analysisfile) {
     watchoptical::FriendTreeCollection dataset({{ratpac, "T"}});
-    ROOT::RDataFrame rdf(dataset.tree());
+    ROOT::RDataFrame rdf(dataset.tree(), {"ev"});
     auto pipeline = rdf
-    .Define("total_charge", [](const std::vector<RAT::DS::EV>& ev){
-        std::vector<double> result;
-        std::transform(ev.begin(), ev.end(), std::back_inserter(result),
-                [](const RAT::DS::EV& e) {
-            return e.GetTotalCharge();
-        });
-        return result;
-        }, {"ev"});
-    pipeline.Snapshot("watchopticalanalysis", analysisfile, {"total_charge"});
+    .Define("total_charge", ::gettotalcharge)
+    .Define("pmt_t", ::getpmt_time)
+    .Define("pmt_q", ::getpmt_charge)
+    .Define("pmt_id", ::getpmt_id)
+    .Define("pmt_eventid", ::getpmt_eventid)
+    ;
+    pipeline.Snapshot("watchopticalanalysis", analysisfile, {
+        "total_charge", "pmt_t", "pmt_q", "pmt_id", "pmt_eventid"
+    });
     return;
 }
 
