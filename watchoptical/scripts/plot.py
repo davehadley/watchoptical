a9457a4c6190bba75fcfe3af1ce3dd9d490ddc5e
David Hadley <d.r.hadley@warwick.ac.uk>
2020-08-10 17:12:24 +0100

    Add requirement for a coincidence.

--- watchoptical/scripts/plot.py
+++ watchoptical/scripts/plot.py
@@ -47,7 +47,10 @@ class TreeTuple(NamedTuple):
 
 def load(analysisfile: AnalysisFile) -> TreeTuple:
     anal = uproot.open(analysisfile.filename)["watchopticalanalysis"].pandas.df(flatten=False)
-    bonsai = uproot.open(analysisfile.producedfrom.bonsaifile)["data"].pandas.df(flatten=False)
+    bonsai = (uproot.open(analysisfile.producedfrom.bonsaifile)["data"]
+              .pandas.df(flatten=False)
+              .set_index(["mcid", "subid"])
+              )
     return TreeTuple(anal, bonsai, analysisfile)
 
 
@@ -76,6 +79,13 @@ def categoryfromfile(file: AnalysisFile) -> str:
     return result
 
 
+def _hascoincidence(data):
+    t = data.groupby("mcid").t
+    count = t.transform("count")
+    dt = t.transform(lambda t: t.max() - t.min())
+    return (count >= 2) & (dt.abs() < 50.0)
+
+
 def selection(data):
     # watchmakers efficiency is based on:
     #             cond = "closestPMT/1000.>%f"%(_d)
@@ -84,18 +94,19 @@ def selection(data):
     #             cond += "&& n9 > %f" %(_n9)
     # with _distance2pmt=1,_n9=8,_dist=30.0,\
     # _posGood=0.1,_dirGood=0.1,_pe=8,_nhit=8,_itr = 1.5
-    return data[(data.closestPMT/1000.0 > 2.0)
+    return data[(data.closestPMT / 1000.0 > 2.0)
                 & (data.good_pos > 0.1)
                 & (data.inner_hit > 4)
                 & (data.veto_hit < 4)
+                & _hascoincidence(data)
                 ]
 
 
 def analysis(tree: TreeTuple) -> bh.Histogram:
     histo = ExposureWeightedHistogram(bh.axis.Regular(25, 0.0, 60.0))
     category = categoryfromfile(tree.analysisfile)
-    histo.fill(category, tree.exposure, tree.bonsai.n9.array)
-    #histo.fill(category, tree.exposure, selection(tree.bonsai).closestPMT.array)
+    #histo.fill(category, tree.exposure, tree.bonsai.n9.array)
+    histo.fill(category, tree.exposure, selection(tree.bonsai).n9.array)
     return histo
 
 
