6f977d41c0c182d1b608e003c9f5312fb3a41d0e
David Hadley <d.r.hadley@warwick.ac.uk>
2020-09-25 15:06:28 +0100

    Improve script for generating watchmakers sensitvity.

--- watchoptical/scripts/runsensitivityanalysis.py
+++ watchoptical/scripts/runsensitivityanalysis.py
@@ -3,28 +3,35 @@ from argparse import ArgumentParser, Namespace
 from typing import Any
 from collections import OrderedDict
 
+import dask
+from dask.bag import Bag
+from dask.delayed import Delayed, delayed
+from toolz import curry
+
 from watchoptical.internal.client import ClientType, client
 from watchoptical.internal.utils import expandpath
 from watchoptical.internal.runwatchmakerssensitivityanalysis import WatchMakersSensitivityAnalysisConfig, \
-    runwatchmakerssensitivityanalysis
+    runwatchmakerssensitivityanalysis, WatchMakersSensitivityResult
 
 
 def _parsecml() -> Namespace:
     parser = ArgumentParser(description="Calculate sensitivity with WATCHMAKER")
-    parser.add_argument("--input-directory", "-i", type=str, default=os.getcwd(),
-                        help="Directory containing input WATCHMAN MC files to be included in the sensitivity analysis."
-                        )
-    parser.add_argument("--output-directory", "-o", type=str, default=os.getcwd(),
-                        help="Directory to store generated files. It will be created if it does not exist."
+    parser.add_argument("input_directories", type=str, nargs="+",
+                        help="Directories containing input WATCHMAN MC files to be included in the sensitivity analysis."
                         )
     parser.add_argument("--target", "-t", type=ClientType, choices=list(ClientType),
                         default=ClientType.SINGLE,
                         help="Where to run jobs."
                         )
+    parser.add_argument("--force", "-f", action="store_true",
+                        help="Run jobs even if there is already a pre-existing result stored.")
     return parser.parse_args()
 
 
 def _validatearguments(args):
+    for directory in args.input_directories:
+        if not os.path.exists(directory):
+            raise ValueError(f"directory {directory} does not exist")
     return
 
 
@@ -36,13 +43,14 @@ def _wrapindict(key: str, value: Any):
 
 
 def _run(args):
-    if not os.path.exists(args.directory):
-        os.makedirs(args.directory, exist_ok=True)
     with client(args.target):
-        config = WatchMakersSensitivityAnalysisConfig(inputdirectory=expandpath(args.input_directory),
-                                                      outputdirectory=expandpath(args.output_directory),
-                                                      )
-        runwatchmakerssensitivityanalysis(config).compute()
+        dask.bag.from_sequence(args.input_directories).map(_processdir(force=args.force)).compute()
+
+
+@curry
+def _processdir(directory: str, force: bool = False) -> WatchMakersSensitivityResult:
+    config = WatchMakersSensitivityAnalysisConfig(inputdirectory=expandpath(directory))
+    return runwatchmakerssensitivityanalysis(config, force=force)
 
 
 def main():
