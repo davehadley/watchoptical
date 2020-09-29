eca283e33d260e564fe68f69256c8720f120626b
David Hadley <d.r.hadley@warwick.ac.uk>
2020-09-29 15:30:30 +0100

    Add script to inspect data files.

--- watchoptical/internal/utils.py
+++ watchoptical/internal/utils.py
@@ -64,16 +64,23 @@ def temporaryworkingdirectory() -> str:
     finally:
         os.chdir(cwd)
 
+
 def hashfromstrcol(values: Iterable[str]) -> str:
     h = hashlib.md5()
     for s in values:
         h.update(s.encode())
     return h.hexdigest()
 
+
 DEFAULT_DBNAME = "watchoptical.shelve.db"
 
 
-def shelvedcall(key: str, f: Callable, *args, dbname: str=DEFAULT_DBNAME, forcecall: bool=False, **kwargs):
+def shelvedget(key: str, dbname: str = DEFAULT_DBNAME):
+    with shelve.open(dbname) as db:
+        return db[key]
+
+
+def shelvedcall(key: str, f: Callable, *args, dbname: str = DEFAULT_DBNAME, forcecall: bool = False, **kwargs):
     with shelve.open(dbname) as db:
         if key in db and not forcecall:
             return db[key]
@@ -83,12 +90,13 @@ def shelvedcall(key: str, f: Callable, *args, dbname: str=DEFAULT_DBNAME, forcec
             return result
 
 
-def shelveddecorator(keyfunc: Callable, dbname: str=DEFAULT_DBNAME):
+def shelveddecorator(keyfunc: Callable, dbname: str = DEFAULT_DBNAME):
     def g(f: Callable):
-        def h(*args, forcecall:bool=False, **kwargs):
+        def h(*args, forcecall: bool = False, **kwargs):
             key = keyfunc(*args, **kwargs)
             return shelvedcall(key, f, *args, dbname=dbname, forcecall=forcecall, **kwargs)
         return h
+
     return g
 
 
