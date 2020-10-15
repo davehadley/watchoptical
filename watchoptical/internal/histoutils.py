4dbdb00d8971908f8554ddfb5f2ec59a8dbeabb8
David Hadley <davehadley@users.noreply.github.com>
2020-10-15 13:48:43 +0100

    Trying to make plots for talk.

--- watchoptical/internal/histoutils.py
+++ watchoptical/internal/histoutils.py
@@ -14,6 +14,7 @@ from typing import (
 )
 
 import boost_histogram as bh
+import matplotlib.pyplot as plt
 import mplhep
 import numpy as np
 from matplotlib.axes import Axes
@@ -70,16 +71,17 @@ def categoryhistplot(
         Callable[[CategoryHistogram.Item], bh.Histogram]
     ] = None,
     formatlabel: Optional[Callable[[CategoryHistogram.Item], str]] = None,
+    ax: Optional[Axes] = None,
     **kwargs: Any,
-) -> Optional[Axes]:
-    ax = None
+) -> Axes:
+    ax = ax if ax is not None else plt.gca()
     formatlabel = formatlabel if formatlabel is not None else lambda x: x.category
     transformhistogram = (
         transformhistogram if transformhistogram is not None else lambda x: x.histogram
     )
     for item in hist:
         histogram = transformhistogram(item)
-        ax = mplhep.histplot(histogram, label=formatlabel(item), **kwargs)
+        mplhep.histplot(histogram, label=formatlabel(item), **kwargs)
     return ax
 
 
