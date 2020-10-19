fbc7db2909fb65bb814dba8a4d79da99d806294a
David Hadley <davehadley@users.noreply.github.com>
2020-10-19 13:21:45 +0100

    Add separate tests to generate signal and background.

--- tests/integration/test_generatemc.py
+++ tests/integration/test_generatemc.py
@@ -10,17 +10,36 @@ from watchoptical.internal.runwatchmakers import WatchMakersConfig
 
 
 class TestGenerateMC(unittest.TestCase):
-    def test_generatemc(self):
+    def test_generatemc_signal(self):
         with dask.distributed.Client(
             n_workers=1, threads_per_worker=1, memory_limit="4GB"
         ):
             with TemporaryDirectory() as d:
                 jobs = generatemc(
                     GenerateMCConfig(
-                        WatchMakersConfig(directory=d, numevents=1), numjobs=1
+                        WatchMakersConfig(directory=d, numevents=1),
+                        numjobs=1,
+                        # it is slow to run all so just run signal
+                        filenamefilter=lambda n: "IBD_LIQUID_pn" in n,
                     )
                 )
-                # it is slow to run all so just pick one
+                results = jobs.compute()
+                self.assertTrue(len(results) > 0)
+                self.assertTrue(all(os.path.exists(f) for r in results for f in r))
+
+    def test_generatemc_background(self):
+        with dask.distributed.Client(
+            n_workers=1, threads_per_worker=1, memory_limit="4GB"
+        ):
+            with TemporaryDirectory() as d:
+                jobs = generatemc(
+                    GenerateMCConfig(
+                        WatchMakersConfig(directory=d, numevents=1),
+                        numjobs=1,
+                        filenamefilter=lambda n: "IBD_LIQUID_pn" not in n,
+                    )
+                )
+                # it is slow to run all so just pick a random background
                 results = random.choice(jobs.to_delayed()).compute()
                 self.assertTrue(len(results) > 0)
                 self.assertTrue(all(os.path.exists(f) for r in results for f in r))
