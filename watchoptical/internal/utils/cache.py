3fdd603962d224a4f65b3c9e0c50b6b06777d785
David Hadley <davehadley@users.noreply.github.com>
2020-10-23 17:23:20 +0100

    Test cache.

--- watchoptical/internal/utils/cache.py
+++ watchoptical/internal/utils/cache.py
@@ -1,11 +1,14 @@
-import shelve
-from typing import Any, Callable
+from contextlib import AbstractContextManager
+from typing import Any, Callable, Iterator, MutableMapping
+
+import cloudpickle
+from sqlitedict import SqliteDict
 
 DEFAULT_DBNAME = "watchoptical_cache.db"
 
 
 def cacheget(key: str, dbname: str = DEFAULT_DBNAME) -> Any:
-    with shelve.open(dbname) as db:
+    with Cache(dbname) as db:
         return db[key]
 
 
@@ -17,7 +20,7 @@ def cachedcall(
     forcecall: bool = False,
     **kwargs,
 ) -> Any:
-    with shelve.open(dbname) as db:
+    with Cache(dbname) as db:
         if key in db and not forcecall:
             return db[key]
         else:
@@ -37,3 +40,35 @@ def cachedcallable(keyfunc: Callable, dbname: str = DEFAULT_DBNAME) -> Callable:
         return h
 
     return g
+
+
+class Cache(MutableMapping[str, Any], AbstractContextManager):
+    def __init__(self, dbname=DEFAULT_DBNAME):
+        self._backing = SqliteDict(
+            dbname,
+            autocommit=True,
+            encode=cloudpickle.dumps,
+            decode=cloudpickle.loads,
+        )
+
+    def __setitem__(self, k: str, v: Any) -> None:
+        self._backing[k] = v
+
+    def __delitem__(self, k: str) -> None:
+        del self._backing[k]
+
+    def __getitem__(self, k: str) -> Any:
+        return self._backing[k]
+
+    def __len__(self) -> int:
+        return len(self._backing)
+
+    def __iter__(self) -> Iterator[str]:
+        return iter(self._backing)
+
+    def __enter__(self):
+        self._backing.__enter__()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._backing.__exit__()
