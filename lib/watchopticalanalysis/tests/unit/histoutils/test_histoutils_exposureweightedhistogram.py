b8c6030542aa86ec0a90519d24a77ae38ed06028
David Hadley <davehadley@users.noreply.github.com>
2021-01-22 14:16:44 +0000

    Oops... botched refactor attempt.

--- lib/watchopticalanalysis/tests/unit/histoutils/test_histoutils_exposureweightedhistogram.py
+++ lib/watchopticalanalysis/tests/unit/histoutils/test_histoutils_exposureweightedhistogram.py
@@ -3,7 +3,7 @@ import unittest
 import boost_histogram as bh
 import numpy as np
 
-from watchopticalmc.internal.histoutils import ExposureWeightedHistogram
+from watchopticalanalysis.internal.histoutils import ExposureWeightedHistogram
 
 
 class TestHistoUtilsExposureWeightedHistogram(unittest.TestCase):
