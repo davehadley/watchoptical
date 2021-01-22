63216959fca637b460a7e5af9a5bede151a0290e
David Hadley <davehadley@users.noreply.github.com>
2021-01-22 13:02:21 +0000

    test dependency.

--- lib/watchopticalanalysis/tests/test_watchopticalanalysis.py
+++ lib/watchopticalanalysis/tests/test_watchopticalanalysis.py
@@ -3,3 +3,7 @@ from watchopticalanalysis import __version__
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_import_watchoptical_mc():
+    import watchopticalmc
+    assert watchopticalmc 
