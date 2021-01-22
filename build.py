175c8d666aa706c7a22901df29a4af3e2632c0a1
David Hadley <davehadley@users.noreply.github.com>
2021-01-22 17:50:33 +0000

    Add poetry lock files.

--- build.py
+++ build.py
@@ -48,7 +48,7 @@ def buildbonsai():
 def buildwatchoptical():
     run(["pip", "install", "--no-input", "-e", "./lib/watchopticalcpp"], title="Install watchopticalcpp")
     run(["poetry", "install"], title="Install watchopticalutils", cwd="./lib/watchopticalutils")
-    run(["poetry", "install", "./lib/watchopticalmc"], title="Install watchopticalmc")
+    run(["poetry", "install"], title="Install watchopticalmc", cwd="./lib/watchopticalmc")
     run(["poetry", "install"], title="Install watchopticalanalysis", cwd="./lib/watchopticalanalysis")
 
 
