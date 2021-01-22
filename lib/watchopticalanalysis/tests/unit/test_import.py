bacd1ca746593293db1578897d058b356d89a64e
David Hadley <davehadley@users.noreply.github.com>
2021-01-22 12:48:42 +0000

    Run poetry new.

--- lib/watchopticalanalysis/tests/unit/test_import.py
+++
@@ -1,9 +0,0 @@
-def test_import_watchopticalanalysis():
-    import watchopticalanalysis
-    _ = watchopticalanalysis # make linter happy
-    return
-
-def test_import_watchopticalmc():
-    import watchopticalmc
-    _ = watchopticalmc # make linter happy
-    return
