ecf6e30c72638c7d45e4273c20a19d6a26fa1dc1
David Hadley <davehadley@users.noreply.github.com>
2021-01-25 16:25:46 +0000

    add more tests.

--- lib/watchopticalanalysis/watchopticalanalysis/process.py
+++ lib/watchopticalanalysis/watchopticalanalysis/process.py
@@ -7,34 +7,27 @@ import operator
 from itertools import starmap
 
 T = TypeVar('T')
+U = TypeVar('U')
 
-class Algorithm(ABC, Generic[T]):
+class Algorithm(ABC, Generic[T, U]):
     @abstractmethod
     def apply(self, data: AnalysisEventTuple) -> T:
         pass
     
     @abstractmethod
-    def finish(self, result: T) -> None:
+    def finish(self, result: T) -> U:
         pass
 
 def process(algorithms: Iterable[Algorithm], dataset: Bag, force: bool=False):
+    algorithms = list(algorithms)
     def fold(*args, **kwargs):
-        print(f"fold-> {args} {kwargs}")
         return tuple(left + right for left, right in zip(*args))
     def finish(*args, **kwargs):
         print(f"finish-> {args} {kwargs}")
         return tuple(alg.finish(r) for (alg, r) in zip(algorithms, args)) 
     reduced = (dataset
         .map(lambda data: tuple(alg.apply(data) for alg in algorithms))
-        #.map(sumlist)
-        #.reduction(lambda x: print(f"x is: {list(x)}"), lambda y: print(f"y is {list(y)}"))
-        #.compute())
-        #.reduction(lambda row: zip(*row), sumlist).compute())
-        .fold(fold, out_type=Bag)
-        .map(finish)
-        )
-    print("DEBUG", reduced)
-    #tuple(alg.finish(result) for alg in algorithms)
-    result = reduced.compute()
-    assert len(result) == 1
-    return result[0]
+        .fold(fold).compute())
+    result = tuple(a.finish(r) for r, a in zip(reduced, algorithms))
+    assert len(result) == len(algorithms)
+    return result
