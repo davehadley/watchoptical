de570570e22b7fd97e586a5ded6ba03d817bf950
David Hadley <davehadley@users.noreply.github.com>
2021-01-25 16:25:46 +0000

    add more tests.

--- lib/watchopticalanalysis/tests/unit/test_run.py
+++ lib/watchopticalanalysis/tests/unit/test_run.py
@@ -1,17 +1,48 @@
 from watchopticalanalysis.process import Algorithm, process
 from watchopticalmc import AnalysisEventTuple
 import dask.bag
+from typing import Any
 
-class Unit(Algorithm[int]):
+class RetInt(Algorithm[int, int]):
+
+    def __init__(self, value: int=1):
+        super().__init__()
+        self.value = value
+
     def apply(self, data: AnalysisEventTuple) -> int:
-        return 1
+        return self.value
     
-    def finish(self, result: int) -> None:
-        print(f"DEBUG finish {result}")
+    def finish(self, result: int) -> int:
         return result
 
-def test_process():
-    algs = [Unit(),]
+class Identity(Algorithm[Any, Any]):
+
+    def apply(self, data: AnalysisEventTuple) -> Any:
+        return data
+    
+    def finish(self, result: int) -> Any:
+        return result
+
+def test_process_no_alg():
+    algs = []
     dataset = dask.bag.from_sequence([None, None, None, None])
     result = process(algs, dataset)
+    assert result == tuple()
+
+def test_process_one_alg():
+    algs = [RetInt(),]
+    dataset = dask.bag.from_sequence([1, 2, 3, 4])
+    result = process(algs, dataset)
     assert result == (4, )
+
+def test_process_two_alg():
+    algs = [RetInt(1), RetInt(2),]
+    dataset = dask.bag.from_sequence(["A", "B", "C", "D"])
+    result = process(algs, dataset)
+    assert result == (4, 8)
+
+def test_process_two_alg_with_data():
+    algs = [Identity(), Identity(),]
+    dataset = dask.bag.from_sequence(["A", "B", "C", "D"])
+    result = process(algs, dataset)
+    assert result == ("ABCD", "ABCD")
