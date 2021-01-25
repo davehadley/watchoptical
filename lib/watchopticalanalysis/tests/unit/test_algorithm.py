d66cfba306d0b9c2b551f731eb302c12f9d7e5fd
David Hadley <davehadley@users.noreply.github.com>
2021-01-25 17:57:19 +0000

    Failing tests

--- lib/watchopticalanalysis/tests/unit/test_algorithm.py
+++ lib/watchopticalanalysis/tests/unit/test_algorithm.py
@@ -1,27 +1,43 @@
-from watchopticalanalysis.algorithm import Algorithm, apply_algorithms
-from watchopticalmc import AnalysisEventTuple
-import dask.bag
 from typing import Any
 
-class RetInt(Algorithm[int, int]):
+import dask.bag
+from watchopticalanalysis.algorithm import (
+    Algorithm,
+    apply_algorithms,
+    cached_apply_algorithms,
+)
+from watchopticalmc import AnalysisEventTuple
+from watchopticalutils.client import ClientType, client
 
-    def __init__(self, value: int=1):
+# from watchopticalutils.client import Client, client
+
+
+class RetInt(Algorithm[int, int]):
+    def __init__(self, value: int = 1):
         super().__init__()
         self.value = value
 
     def apply(self, data: AnalysisEventTuple) -> int:
         return self.value
-    
+
     def finish(self, result: int) -> int:
         return result
 
+
 class Identity(Algorithm[Any, Any]):
+    def __init__(self):
+        super().__init__()
+        self.apply_count = 0
+        self.finish_count = 0
 
     def apply(self, data: AnalysisEventTuple) -> Any:
+        self.apply_count += 1
         return data
-    
+
     def finish(self, result: int) -> Any:
+        self.finish_count += 1
         return result
+
 
 def test_process_no_alg():
     algs = []
@@ -29,24 +45,53 @@ def test_process_no_alg():
     result = apply_algorithms(algs, dataset)
     assert result == tuple()
 
+
 def test_process_one_alg():
-    algs = [RetInt(),]
+    algs = [
+        RetInt(),
+    ]
     dataset = dask.bag.from_sequence([1, 2, 3, 4])
     result = apply_algorithms(algs, dataset)
-    assert result == (4, )
+    assert result == (4,)
+
 
 def test_process_two_alg():
-    algs = [RetInt(1), RetInt(2),]
+    algs = [
+        RetInt(1),
+        RetInt(2),
+    ]
     dataset = dask.bag.from_sequence(["A", "B", "C", "D"])
     result = apply_algorithms(algs, dataset)
     assert result == (4, 8)
 
+
 def test_process_two_alg_with_data():
-    algs = [Identity(), Identity(),]
+    algs = [
+        Identity(),
+        Identity(),
+    ]
     dataset = dask.bag.from_sequence(["A", "B", "C", "D"])
     result = apply_algorithms(algs, dataset)
     assert result == ("ABCD", "ABCD")
 
+
 def test_cached_algorithm():
-    algs = [Identity(), Identity()]
-    cached_apply_algorithms(algs, )
+    with client(ClientType.SINGLE):
+        algs1 = [Identity(), Identity()]
+        dataset = dask.bag.from_sequence(["A", "B", "C", "D"])
+        result1 = cached_apply_algorithms(algs1, dataset)
+        algs2 = [Identity(), Identity()]
+        result2 = cached_apply_algorithms(algs2, dataset)
+        assert all(
+            [
+                algs1[0].apply_count == 4,
+                algs1[1].apply_count == 4,
+                algs2[0].apply_count == 0,
+                algs2[1].apply_count == 0,
+                algs1[0].finish_count == 1,
+                algs1[1].finish_count == 1,
+                algs2[0].finish_count == 1,
+                algs2[1].finish_count == 1,
+            ]
+        )
+        assert result1 == result2
--- lib/watchopticalanalysis/watchopticalanalysis/algorithm.py
+++ lib/watchopticalanalysis/watchopticalanalysis/algorithm.py
@@ -1,45 +1,46 @@
-from typing import TypeVar, Callable, Generic, Iterable, Tuple
-from watchopticalmc import AnalysisEventTuple
-from watchopticalutils.collectionutils import sumlist
 from abc import ABC, abstractmethod
-from dask.bag import Bag
-import operator
-from itertools import starmap
+from typing import Generic, Iterable, Optional, Tuple, TypeVar
 
-T = TypeVar('T')
-U = TypeVar('U')
+from dask.bag import Bag
+from watchopticalmc import AnalysisEventTuple
+from watchopticalutils.cache import Cache
+
+T = TypeVar("T")
+U = TypeVar("U")
+
 
 class Algorithm(ABC, Generic[T, U]):
     @abstractmethod
     def apply(self, data: AnalysisEventTuple) -> T:
         pass
-    
+
     @abstractmethod
     def finish(self, result: T) -> U:
         pass
 
+
 def apply_algorithms(algorithms: Iterable[Algorithm], dataset: Bag) -> Tuple:
     algorithms = list(algorithms)
+
     def fold(*args, **kwargs):
         return tuple(left + right for left, right in zip(*args))
+
     def finish(*args, **kwargs):
         return tuple(alg.finish(r) for (alg, r) in zip(algorithms, args))
-    reduced = (dataset
-        .map(lambda data: tuple(alg.apply(data) for alg in algorithms))
-        .fold(fold).compute())
+
+    reduced = (
+        dataset.map(lambda data: tuple(alg.apply(data) for alg in algorithms))
+        .fold(fold)
+        .compute()
+    )
     result = tuple(a.finish(r) for r, a in zip(reduced, algorithms))
     assert len(result) == len(algorithms)
     return result
 
-def apply_cached_algorithms(cache: algorithms: Iterable[Algorithm], dataset: Bag) -> Tuple:
-    algorithms = list(algorithms)
-    def fold(*args, **kwargs):
-        return tuple(left + right for left, right in zip(*args))
-    def finish(*args, **kwargs):
-        return tuple(alg.finish(r) for (alg, r) in zip(algorithms, args))
-    reduced = (dataset
-        .map(lambda data: tuple(alg.apply(data) for alg in algorithms))
-        .fold(fold).compute())
-    result = tuple(a.finish(r) for r, a in zip(reduced, algorithms))
-    assert len(result) == len(algorithms)
-    return result
+
+def cached_apply_algorithms(
+    algorithms: Iterable[Algorithm], dataset: Bag, cache: Optional[Cache] = None
+) -> Tuple:
+    # if cache is None:
+    #    cache = Cache()
+    return apply_algorithms(algorithms, dataset)
