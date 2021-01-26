5a97e3551b910b65fe3d153cd681cc3d94541dc9
David Hadley <davehadley@users.noreply.github.com>
2021-01-26 11:45:50 +0000

    Tests pass.

---
+++ lib/watchopticalanalysis/watchopticalanalysis/algs/__init__.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+from .test import Test
+
+
+class AlgDefs(Enum):
+    test = Test()
+    # hist = partial(plothist)
+    # scatter = partial(plotscatter)
+    # attenuation = partial(plotattenuation)
+    # dumpselectionstats = partial(dumpselectiontables)
