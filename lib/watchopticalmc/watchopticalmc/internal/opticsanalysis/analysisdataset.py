74a9a2bd6f892754c4d3484586c43ff64ec4b598
David Hadley <davehadley@users.noreply.github.com>
2021-01-26 11:53:03 +0000

    Msising from previous commit.

--- lib/watchopticalmc/watchopticalmc/internal/opticsanalysis/analysisdataset.py
+++ lib/watchopticalmc/watchopticalmc/internal/opticsanalysis/analysisdataset.py
@@ -5,6 +5,8 @@ import cloudpickle
 from watchopticalmc.internal.generatemc.mctoanalysis import AnalysisFile
 from watchopticalmc.internal.generatemc.wmdataset import WatchmanDataset
 
+_DEFAULT_FILE_NAME = "analysisdataset.pickle"
+
 
 class AnalysisDataset(NamedTuple):
     inputfiles: List[Path]
@@ -14,9 +16,13 @@ class AnalysisDataset(NamedTuple):
 
     @classmethod
     def load(cls, path: Path) -> "AnalysisDataset":
+        if path.is_dir():
+            path = path / _DEFAULT_FILE_NAME
         with open(path, "rb") as fp:
             return cloudpickle.load(fp)
 
     def write(self, path: Path) -> None:
+        if path.is_dir():
+            path = path / _DEFAULT_FILE_NAME
         with open(path, "wb") as fp:
             cloudpickle.dump(self, fp)
