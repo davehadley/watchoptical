d9908b1bbf7a6fe77bc6712b2d095afca31cc2bd
David Hadley <davehadley@users.noreply.github.com>
2021-01-27 16:42:27 +0000

    Improve algoirthm tests.

--- lib/watchopticalanalysis/tests/unit/test_run.py
+++ lib/watchopticalanalysis/tests/unit/test_run.py
@@ -1,29 +1,32 @@
+from pathlib import Path
 from subprocess import run
+from tempfile import TemporaryDirectory
 
 import pytest
 from watchopticalanalysis.algs import AlgDefs
 from watchopticalmc import AnalysisDataset
-from watchopticalutils.filepathutils import temporaryworkingdirectory
 
 
-def test_run_noop(signaldatasetfixture: AnalysisDataset):
-    with temporaryworkingdirectory():
+def test_run_timestamp(signaldatasetfixture: AnalysisDataset):
+    with TemporaryDirectory() as dir:
         run(
             [
                 "python3",
                 "-m",
                 "watchopticalanalysis",
                 str(signaldatasetfixture.directory),
-                "--alg=test",
+                "--alg=timestamp",
                 "--client=single",
+                f"--output={dir}",
             ],
             check=True,
         )
+        assert (Path(dir) / "plots" / "timestamp.txt").exists()
 
 
 @pytest.mark.parametrize("alg", list(AlgDefs))
 def test_run_each_alg(signaldatasetfixture: AnalysisDataset, alg: AlgDefs):
-    with temporaryworkingdirectory():
+    with TemporaryDirectory() as dir:
         run(
             [
                 "python3",
@@ -32,6 +35,10 @@ def test_run_each_alg(signaldatasetfixture: AnalysisDataset, alg: AlgDefs):
                 str(signaldatasetfixture.directory),
                 f"--alg={alg.name}",
                 "--client=single",
+                f"--output={dir}",
             ],
             check=True,
         )
+        assert (Path(dir) / "plots").exists()
+        # check it creates at least 1 new file
+        assert next((Path(dir) / "plots").glob("**/*"))
