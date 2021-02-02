cd9d6d5d658ec928adb008ace435baf6c2bd389a
David Hadley <davehadley@users.noreply.github.com>
2021-02-02 14:18:49 +0000

    hmm

--- lib/watchopticalmc/tests/integration/test_zeroatt_gives_no_light.py
+++ lib/watchopticalmc/tests/integration/test_zeroatt_gives_no_light.py
@@ -1,5 +1,11 @@
+from watchopticalutils.filepathutils import temporaryworkingdirectory
+from subprocess import run
+import numpy as np
+import uproot
+
 def test_zeroatt_gives_no_light():
-    with TemporaryDirectory() as dir:
-        run(["python", "-m", "watchopticalmc", "--num-jobs=1", "--num-events-per-job=10", "--attenuation=0.0", "--client=single", "--signal-only", f"--directory={dir}"])
+    with temporaryworkingdirectory() as dir:
+        run(["/root/watchoptical/env/watchoptical/bin/python", "-m", "watchopticalmc", "--num-jobs=1", "--num-events-per-job=10", "--attenuation=0.0", "--client=single", "--signal-only", f"--directory={dir}"])
         qmean = np.average(np.concatenate(uproot.lazyarray("watchopticalanalysis*.root", "watchopticalanalysis", "total_charge")))
+        breakpoint()
         assert qmean < 10.0
