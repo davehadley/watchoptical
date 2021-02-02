7b7fed3e39cdbea7524a28a201d1118f5bb80310
David Hadley <davehadley@users.noreply.github.com>
2021-02-02 15:59:11 +0000

    Remove unneeded files.

--- external/copy_from_temp/run.sh
+++
@@ -1,5 +0,0 @@
-
-source /root/watchoptical/build/rat-pac/../../env.sh
-TMPNAME=$(date +%s%N)
-rat attenuation_1e-12_scattering_None.mac _linktoconfig.mac  mac/detector_Watchman.mac mac/process_Watchman.mac mac/phys_heysham_background.mac mac/geo_LIQUID.mac mac/rates_Watchman_heysham_background_LIQUID_pn_ibd.mac -o test.root -l test.log'
-
